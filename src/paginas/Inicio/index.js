76b989163f2d17a1cd33592e6cc73be9b2c2c27a
DGusFr <diegogustavo.fr@gmail.com>
2023-04-30 15:08:10 -0300

    changes

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -3,6 +3,7 @@ import styles from './Inicio.module.css';
 import posts from 'json/posts.json';
 import Post from "componentes/Post";
 
+//parte responsavél por gerar a aba post no inicio
 export default function Inicio() {
     return (
         <ul className={styles.posts}>
